840c4611189f3ee07c003991bfe6483d753f6d93
sofiaversoza <sofversoza@gmail.com>
2022-08-08 17:25:08 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+      <h1>Homepage</h1>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque ipsum aut suscipit quam
+				perspiciatis iusto deleniti officiis nobis, omnis facilis excepturi, cum repellat veritatis,
+				impedit at eius. Libero, cum minima?
+			</p>
+		</div>
+	)
 }
 
-export default App;
+export default App
